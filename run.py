4900483e7808d09b1b8fb92930e13ebfc27f6c6a
trade-hw <102856235+trade-hw@users.noreply.github.com>
2022-11-14 02:17:58 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
-import ccxt 
+import ccxt
 import pprint
 import time
 import datetime
 import pandas as pd
-import larry 
+import larry
 import math
 
 
@@ -14,7 +14,7 @@ with open("../api.txt") as f:
 
 
 binance = ccxt.binance(config={
-    'apiKey': api_key, 
+    'apiKey': api_key,
     'secret': secret,
     'enableRateLimit': True,
     'options': {
@@ -27,18 +27,18 @@ symbol = "BTC/USDT"
 long_target, short_target = larry.cal_target(binance, symbol)
 balance = binance.fetch_balance()
 usdt = balance['total']['USDT']
-op_mode = False 
+op_mode = False
 position = {
     "type": None,
     "amount": 0
-} 
+}
 
 
 def cal_amount(usdt_balance, cur_price):
-    portion = 0.1 
+    portion = 0.1
     usdt_trade = usdt_balance * portion
     amount = math.floor((usdt_trade * 1000000)/cur_price) / 1000000
-    return amount 
+    return amount
 
 
 def enter_position(exchange, symbol, cur_price, long_target, short_target, amount, position):
@@ -56,26 +56,26 @@ def exit_position(exchange, symbol, position):
     amount = position['amount']
     if position['type'] == 'long':
         exchange.create_market_sell_order(symbol=symbol, amount=amount)
-        position['type'] = None 
+        position['type'] = None
     elif position['type'] == 'short':
         exchange.create_market_buy_order(symbol=symbol, amount=amount)
-        position['type'] = None 
+        position['type'] = None
 
 
-while True: 
+while True:
     now = datetime.datetime.now()
 
     if now.hour == 8 and now.minute == 50 and (0 <= now.second < 10):
         if op_mode and position['type'] is not None:
             exit_position(binance, symbol, position)
-            op_mode = False         # 9시 까지는 다시 포지션 진입하지 않음 
+            op_mode = False         # 9시 까지는 다시 포지션 진입하지 않음
 
     # udpate target price
     if now.hour == 9 and now.minute == 0 and (20 <= now.second < 30):
         long_target, short_target = larry.cal_target(binance, symbol)
         balance = binance.fetch_balance()
         usdt = balance['total']['USDT']
-        op_mode = True 
+        op_mode = True
         time.sleep(10)
 
     ticker = binance.fetch_ticker(symbol)
