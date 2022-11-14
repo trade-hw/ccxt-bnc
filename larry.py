70f2a14ff9a52433d26fdb2c711bcce0b20ef9f3
trade-hw <102856235+trade-hw@users.noreply.github.com>
2022-11-15 04:15:23 +0900

    Update larry.py

--- larry.py
+++ larry.py
@@ -24,14 +24,15 @@ def rsi_binance(itv='1h', symbol=symbol):
 
 
 # volatility breakout
-def cal_target(exchange, symbol):
+def cal_target(exchange, symbol):  # 심볼에 대한 ohlcv (지정시간)캔들 데이터 얻기
     data = exchange.fetch_ohlcv(
         symbol=symbol,
-        timeframe='3m',
+        timeframe='3m',  # 지정시간
         since=None,
-        limit=10
+        limit=10  # 몇 개를 얻을 것인지
     )
     
+    # 위 (지정시간)캔들 데이터를, 데이터프레임 객체로 변환
     df = pd.DataFrame(data=data, columns=['datetime', 'open', 'high', 'low', 'close', 'volume'])
     df['datetime'] = pd.to_datetime(df['datetime'], unit='ms')
     df.set_index('datetime', inplace=True)
@@ -40,10 +41,11 @@ def cal_target(exchange, symbol):
     ma5 = df['close'].rolling(5).mean()
     ma200 = df['close'].rolling(200).mean()
     
-    yesterday = df.iloc[-2]
-    today = df.iloc[-1]
+    # 이전 / 현재 데이터로 목표가 계산
+    yesterday = df.iloc[-2]  # 이전
+    today = df.iloc[-1]  # 현재
     
-    #  롱숏 타겟 구하기
+    #  롱 / 숏 목표가 구하기
     long_target = today['open'] + (yesterday['high'] - yesterday['low']) * 0.1
     short_target = today['open'] - (yesterday['high'] - yesterday['low']) * 0.1
     
